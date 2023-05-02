4624c8bf9e6708ed7785587598b5abcde3068be1
Md Jamil Akter <jamilakterup@gmail.com>
2023-05-02 17:08:49 +0600

    update user

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,44 +3,77 @@ import Lottie from "lottie-react";
 import groovyWalkAnimation from "../../../public/login.json";
 import {Link} from 'react-router-dom';
 import {AuthContext} from '../../providers/AuthProvider';
+import {updateProfile} from 'firebase/auth';
 
 const Register = () => {
-    const {SignUpNewUser} = useContext(AuthContext);
     const [error, setError] = useState('');
     const [success, setSuccess] = useState('');
+
+    const {SignUpNewUser} = useContext(AuthContext);
 
     const handleRegisterUser = e => {
         e.preventDefault();
         const form = e.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
         const email = form.email.value;
         const password = form.password.value;
         const confirm = form.confirm.value;
+
+        setError('');
+        setSuccess('');
 
         if (password !== confirm) {
             setError('Password not matched');
             return;
         }
-        setError('')
+        if (password.length < 6) {
+            setError('Password must be at least 6 characters');
+            return;
+        }
+
         SignUpNewUser(email, password)
             .then(result => {
                 const registeredUser = result.user;
                 setSuccess('successfully registered user')
+                updateuserInfo(result.user, name, photo)
                 console.log(registeredUser);
             })
-            .catch(err => console.log(err))
+            .catch(error => setError(error.message));
+
+        const updateuserInfo = (user, name, photo) => {
+            updateProfile(user, {
+                displayName: name,
+                photoURL: photo,
+            })
+                .then()
+                .catch(error => setError(error.message));
+        }
     }
 
     return (
         <div className="hero min-h-screen my-12">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
+                <div className="text-center lg:text-left w-1/2">
                     <h1 className="text-5xl font-bold text-center">Register now!</h1>
                     <Lottie animationData={groovyWalkAnimation} loop={true} />
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <form onSubmit={handleRegisterUser} className="card-body">
                         <p className='text-center text-red-600'>{error}</p>
-                        <p className='text-center text-success'>{setSuccess}</p>
+                        <p className='text-center text-success'>{success}</p>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="Name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input type="url" name='photo' placeholder="photo url" className="input input-bordered" />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -51,7 +84,7 @@ const Register = () => {
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                            <input type="password" name='password' placeholder="6 characters password" className="input input-bordered" required />
                             <label className="label">
                                 <span className="label-text">Confirm Password</span>
                             </label>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,17 +1,27 @@
-import React, {createContext, useState} from 'react';
-import {createUserWithEmailAndPassword, getAuth} from "firebase/auth";
+import React, {createContext, useEffect, useState} from 'react';
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, updateProfile} from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
-    // const [user, setUser] = useState(null);
-    const user = 'jamil'
+    const [user, setUser] = useState(null);
 
     const SignUpNewUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            if (currentUser) {
+                setUser(currentUser);
+            }
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
 
     const authInfo = {
         user,
